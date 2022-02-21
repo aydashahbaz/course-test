0a2bab2da470a2d21726ac22a4c73ffb1055a505
aydashahbaz <aydashahbaz997@gmail.com>
2022-02-21 20:05:59 +0500

    Angular JS Added Successfull!!!

---
+++ Angular JS/fullstack-course5-master/examples/Lecture04/app.js
@@ -0,0 +1,26 @@
+(function(){
+'use strict';
+ 
+    angular.module('myFirstApp', []);
+    .controller('MyFirstController', function() {
+
+    });
+
+
+
+})();
+
+
+
+/*
+(function () {
+'use strict';
+
+angular.module('myFirstApp', [])
+
+.controller('MyFirstController', function () {
+
+});
+
+})();
+*/
